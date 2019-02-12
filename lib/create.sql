426dda202003a0649aa8b56c030713d3781262c0
Victoria B <viktorija.bourassa@gmail.com>
2019-02-12 22:32:13 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE project (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal REAL,
+  start_date TEXT,
+  end_date TEXT
+);
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE
